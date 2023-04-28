3d4b887138b1e4e2fa67c3d585bbcfc0a7953e3f
Kendra Moore <kendra.j.moore3443@gmail.com>
2023-04-28 07:16:22 -0700

    Update
    Added MQTT to device

--- influx_cloud_light_correct.ino
+++ influx_cloud_light_correct.ino
@@ -1,129 +1,154 @@
-//set up sensors library
+#include <Arduino.h>
+//sensor library
 #include <DHT.h>
-#include <Wire.h>
-#include <BH1750.h>
-
-BH1750 lightMeter;
-
-#define DHT_SENSOR_PIN  21 // ESP32 pin GIOP21 connected to DHT11 sensor
-#define DHT_SENSOR_TYPE DHT11
-#define LIGHT_DEVICE TEMT6000
-#define LIGHT_SENSOR_PIN  23
-#define SOIL_SENSOR_PIN  19
-#define CURRENT_DEVICE ESP32
-
+//include Wifi library
+#include <WiFi.h>
+extern "C" {
+  #include "freertos/FreeRTOS.h"
+  #include "freertos/timers.h"
+}
+//MQTT library
+#include <AsyncMqttClient.h>
+//setup sensors connected to device
+#define DHT_SENSOR_PIN  DEVICE_PIN_NUMBER 
+#define DHT_SENSOR_TYPE TEMP_SENSOR_TYPE
+#define SOIL_SENSOR_PIN  SOIL_PIN_NUMBER 
+#define DEVICE DEVICE_NAME
 DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+//connect to wifi
+#define WIFI_SSID SECRET_SSID
+#define WIFI_PASSWORD SECRET_PASSWORD
+// Digital Ocean MQTT Mosquitto Broker
+#define MQTT_HOST IPAddress(ADD_IP_ADDRESS)
+#define MQTT_PORT 1883
+#define MQTT_USERNAME SECRET_MQTT_USERNAME
+#define MQTT_PASSWORD SECRET_MQTT_PASSWORD
+//MQTT topics
+#define MQTT_PUB_TEMP MQTT_TOPIC_ONE 
+#define MQTT_PUB_HUM MQTT_TOPIC_TWO  
+#define MQTT_PUB_SOIL MQTT_TOPIC_THREE
 
-//set up wifi library
-#if defined(ESP32)
-#include <WiFiMulti.h>
-WiFiMulti wifiMulti;
-#define DEVICE "ESP32"
-#elif defined(ESP8266)
-#include <ESP8266WiFiMulti.h>
-ESP8266WiFiMulti wifiMulti;
-#define DEVICE "ESP8266"
-#endif
-
-//setup influx library
-#include <InfluxDbClient.h>
-#include <InfluxDbCloud.h>
-
-// WiFi AP SSID
-#define WIFI_SSID ""
-// WiFi password
-#define WIFI_PASSWORD ""
-// InfluxDB v2 server url, e.g. https://eu-central-1-1.aws.cloud2.influxdata.com (Use: InfluxDB UI -> Load Data -> Client Libraries)
-#define INFLUXDB_URL ""
-// InfluxDB v2 server or cloud API token (Use: InfluxDB UI -> Data -> API Tokens -> Generate API Token)
-#define INFLUXDB_TOKEN ""
-// InfluxDB v2 organization id (Use: InfluxDB UI -> User -> About -> Common Ids )
-#define INFLUXDB_ORG "plant_berkeley"
-// InfluxDB v2 bucket name (Use: InfluxDB UI ->  Data -> Buckets)
-#define INFLUXDB_BUCKET "test"
-
-// Set timezone string according to https://www.gnu.org/software/libc/manual/html_node/TZ-Variable.html
-// Examples:
-//  Pacific Time: "PST8PDT"
-//  Eastern: "EST5EDT"
-//  Japanesse: "JST-9"
-//  Central Europe: "CET-1CEST,M3.5.0,M10.5.0/3"
-#define TZ_INFO "PST8PDT"
-
-//Soil Tools
-int wet = 595;
-int dry = 239;
+// Numbers to Calibrate Soil Tool 
+int wet = 239;
+int dry = 595;
 
 
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
 
-// InfluxDB client instance with preconfigured InfluxCloud certificate
-InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
-//InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
-// Data point
-Point sensor("measure");
+unsigned long previousMillis = 0;   // Stores last time temperature was published
+const long interval = 5000;         // Interval at which to publish sensor readings
 
+int i = 0;
 
-void setup() {
-  Serial.begin(115200);
-  Wire.begin();
-  lightMeter.begin();
-  dht_sensor.begin();
-  Serial.println(F("BH1750 Test begin"));
-  
-  // Setup wifi
-  WiFi.mode(WIFI_STA);
-  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  delay(10000);
+}
 
-  Serial.print("Connecting to wifi");
-  while (wifiMulti.run() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(100);
-  }
-  Serial.println();
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
 
-  // Add tags
-  sensor.addTag("garden", "garden");
-  sensor.addTag("device", DEVICE);
-  sensor.addTag("SSID", WiFi.SSID());
-  // Accurate time is necessary for certificate validation and writing in batches
-  // For the fastest time sync find NTP servers in your area: https://www.pool.ntp.org/zone/
-  // Syncing progress and the time will be printed to Serial.
-  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
-
-  // Check server connection
-  if (client.validateConnection()) {
-    Serial.print("Connected to InfluxDB: ");
-    Serial.println(client.getServerUrl());
-  } else {
-    Serial.print("InfluxDB connection failed: ");
-    Serial.println(client.getLastErrorMessage());
+void WiFiEvent(WiFiEvent_t event) {
+  Serial.printf("[WiFi-event] event: %d\n", event);
+  switch(event) {
+    case SYSTEM_EVENT_STA_GOT_IP:
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+      connectToMqtt();
+      break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+      Serial.println("WiFi lost connection");
+      xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+      xTimerStart(wifiReconnectTimer, 0);
+      break;
   }
 }
 
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+// Do whatever you want when you receive a message Serial.println("Publish received."); Serial.print(" topic: ");
+  Serial.println(topic);
+  Serial.print(" qos: "); 
+  Serial.println(properties.qos); 
+  Serial.print(" dup: "); 
+  Serial.println(properties.dup); 
+  Serial.print(" retain: "); 
+  Serial.println(properties.retain); 
+  Serial.print(" len: "); 
+  Serial.println(len); 
+  Serial.print(" index: "); 
+  Serial.println(index); 
+  Serial.print(" total: "); 
+  Serial.println(total);
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.print("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void setup() {
+  Serial.begin(115200);
+  dht_sensor.begin();
+  Serial.println();
+
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+
+  WiFi.onEvent(WiFiEvent);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  mqttClient.setCredentials(MQTT_USERNAME, MQTT_PASSWORD);
+  connectToWifi();
+}
+
 void loop() {
-  // Clear fields for reusing the point. Tags will remain untouched
   sensor.clearFields();
-  float lux = lightMeter.readLightLevel();
-  Serial.print("Light: ");
-  Serial.print(lux);
-  Serial.println(" lx");
   float humi  = dht_sensor.readHumidity();
+  //remove true to get temperature in Celsuis
   float f = dht_sensor.readTemperature(true);
-  int light_num = analogRead(19);
   int soil_num = analogRead(A0);
-  int soil_final_num = map(soil_num, wet, dry, 100, 0);
-  Serial.print(soil_final_num);
-  Serial.println("%");
-
-
- 
-  // Store measured value into point
-  // Report RSSI of currently connected network
+  int soil = map(soil_num, wet, dry, 100, 0);
   sensor.addField("rssi", WiFi.RSSI());
-  sensor.addField("light", light_num);
   sensor.addField("temp", f);
-  sensor.addField("soil", soil_final_num);
-
+  sensor.addField("humi", humi);
+  sensor.addField("soil", soil);
   if ( isnan(humi)) {
     Serial.println("Failed to read from DHT sensor!");
   } else {
@@ -132,28 +157,24 @@ void loop() {
     Serial.println("%");
     Serial.print("Temp: ");
     Serial.println(f);
-    Serial.println("F");
-    Serial.println(light_num);
-    Serial.print("Light");
-    Serial.println(soil_final_num);
-    Serial.print("soil");
+    Serial.print("soil: ");
+    Serial.println(soil);
   }
-
-  // Print what are we exactly writing
-  Serial.print("Writing: ");
-  Serial.println(sensor.toLineProtocol());
-
-  // Check WiFi connection and reconnect if needed
-  if (wifiMulti.run() != WL_CONNECTED) {
-    Serial.println("Wifi connection lost");
+  unsigned long currentMillis = millis();
+  // Every X number of seconds (interval = 5 seconds) 
+  // it publishes a new MQTT message
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(f).c_str());
+    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_TEMP, packetIdPub1);
+    Serial.printf("Message: %.2f \n", f);
+    uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(humi).c_str());
+    Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_HUM, packetIdPub2);
+    Serial.printf("Message: %.2f \n", humi);
+    uint16_t packetIdPub3 = mqttClient.publish(MQTT_PUB_SOIL, 1, true, String(soil).c_str());
+    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_SOIL, packetIdPub3);
+    Serial.printf("Message: %.3f \n", soil);
   }
-
-  // Write point
-  if (!client.writePoint(sensor)) {
-    Serial.print("InfluxDB write failed: ");
-    Serial.println(client.getLastErrorMessage());
-  }
-
-  Serial.println("Wait 10s");
-  delay(16000);
+  delay(10000);
 }
